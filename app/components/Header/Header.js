f0db551269f1f47df24b9c922797c9221ee34e50
“Mujtaba” <mmuujji@gmail.com>
2018-02-28 15:21:22 +0500

    production build

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -9,29 +9,29 @@ class Header extends React.Component { // eslint-disable-line react/prefer-state
       <header className="header">
         <nav className="navbar navbar-expand-lg navbar-light">
           <div className="container-fluid">
-            <a className="navbar-brand" onClick={() => this.props.history.push('/')}><img src={logo} alt="Corl" id="logo" width="140" /></a>
+            <a className="navbar-brand" href="/" ><img src={logo} alt="Corl" id="logo" width="140" /></a>
             <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
-                  <a className="nav-link" href="" onClick={() => this.props.history.push('/crltoken')}>Whitepaper</a>
+                  <a className="nav-link" href="/crltoken" >Whitepaper</a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="" onClick={() => this.props.history.push('/airdrop')}>Airdrop Program</a>
+                  <a className="nav-link" href="/airdrop" >Airdrop Program</a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="https://bitcointalk.org/index.php?topic=2876865.msg29553585#msg29553585" target="_blank">Bounty Program</a>
+                  <a className="nav-link" href="https://bitcointalk.org/index.php?topic=2876865.msg29553585/msg29553585" target="_blank">Bounty Program</a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="" onClick={() => this.props.history.push('/companies')}>For Companies</a>
+                  <a className="nav-link" href="/companies" >For Companies</a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="" onClick={() => this.props.history.push('/investor')}>For Investors</a>
+                  <a className="nav-link" href="/investor" >For Investors</a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="" onClick={() => this.props.history.push('/about')}>About</a>
+                  <a className="nav-link" href="/about" >About</a>
                 </li>
                 <li className="nav-item">
                   <a className="nav-link" href="" href="https://blog.corl.io/">Blog</a>
